4b9d8dd38cf2b40fb36b4b830a9c3e03113b6d7a
Ausar <lujunfeng@junfeng.lu>
2024-08-27 16:43:24 +0800

    improved compile script
    Signed-off-by: Ausar <lujunfeng@junfeng.lu>

---
+++ compile_and_install.sh
@@ -0,0 +1,6 @@
+cd src/nrn_modify/
+./compile_and_install.sh
+cd ../../
+cd src/DeepDendrite/
+./compile_and_install.sh
+
--- src/nrn_modify/compile_and_install.sh
+++ src/nrn_modify/compile_and_install.sh
@@ -1,6 +1,8 @@
+#for nvhpc docker:use openmpi instead of nvidia's
 export PATH=/usr/bin:$PATH
 export CC=gcc
 export CXX=g++
+autoreconf -ivf
 ./configure --prefix $PWD/../../install \
 	--without-iv \
 	--with-paranrn \
